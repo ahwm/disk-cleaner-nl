ecfe34c1ba03eddb2a4995c3bf20fbd18318ed95
rjmoerland <rjmoerland@bd5d69a4-6419-0410-a1cd-843bcb727202>
2009-05-03 12:13:33 +0000

    Added license notification to source code in wxlistctrllog.cpp/.h

--- diskcleaner/trunk/src/gui/wxlistctrllog.h
+++ diskcleaner/trunk/src/gui/wxlistctrllog.h
@@ -1,3 +1,16 @@
+/*
+** Copyright (C) 2009 Robert Moerland
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the wxWindows Library Licence, Version 3.1
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** wxWindows Library Licence for more details.
+**
+*/
+
 #ifndef WXLISTBOXLOG_H
 #define WXLISTBOXLOG_H
 

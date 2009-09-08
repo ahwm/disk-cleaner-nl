789a1d2ca537ed0bf3ad178eea296069ffd6b6cc
rjmoerland <rjmoerland@bd5d69a4-6419-0410-a1cd-843bcb727202>
2009-09-08 21:41:00 +0000

    Fixed a bug in the Firefox plugin (firefox.cpp) that prevented the history and cookie files from being removed. The file name was always wrong.

--- diskcleaner/trunk/src/gui/version.h
+++ diskcleaner/trunk/src/gui/version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "05";
+	static const char DATE[] = "08";
 	static const char MONTH[] = "09";
 	static const char YEAR[] = "2009";
 	static const double UBUNTU_VERSION_STYLE = 9.09;
@@ -16,14 +16,14 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 6;
-	static const long BUILD = 1263;
-	static const long REVISION = 6922;
+	static const long BUILD = 1273;
+	static const long REVISION = 6980;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 2351;
-	#define RC_FILEVERSION 1,6,1263,6922
-	#define RC_FILEVERSION_STRING "1, 6, 1263, 6922\0"
-	static const char FULLVERSION_STRING[] = "1.6.1263.6922";
+	static const long BUILDS_COUNT = 2371;
+	#define RC_FILEVERSION 1,6,1273,6980
+	#define RC_FILEVERSION_STRING "1, 6, 1273, 6980\0"
+	static const char FULLVERSION_STRING[] = "1.6.1273.6980";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 0;

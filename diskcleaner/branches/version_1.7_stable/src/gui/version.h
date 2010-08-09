aa17d5b24b7877b28e3cc25c7c0e79442ffdee4d
rjmoerland <rjmoerland@bd5d69a4-6419-0410-a1cd-843bcb727202>
2010-08-09 07:11:36 +0000

    Fixed a few minor memory leaks, detected by CppCheck

--- diskcleaner/branches/version_1.7_stable/src/gui/version.h
+++ diskcleaner/branches/version_1.7_stable/src/gui/version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "07";
+	static const char DATE[] = "09";
 	static const char MONTH[] = "08";
 	static const char YEAR[] = "2010";
 	static const char UBUNTU_VERSION_STYLE[] = "10.08";
@@ -16,14 +16,14 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 7;
-	static const long BUILD = 1615;
-	static const long REVISION = 8777;
+	static const long BUILD = 1616;
+	static const long REVISION = 8779;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT = 2944;
-	#define RC_FILEVERSION 1,7,1615,8777
-	#define RC_FILEVERSION_STRING "1, 7, 1615, 8777\0"
-	static const char FULLVERSION_STRING[] = "1.7.1615.8777";
+	#define RC_FILEVERSION 1,7,1616,8779
+	#define RC_FILEVERSION_STRING "1, 7, 1616, 8779\0"
+	static const char FULLVERSION_STRING[] = "1.7.1616.8779";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 0;

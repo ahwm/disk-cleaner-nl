58825f8d941b570019c65f34d12edcb5eb8f0384
rjmoerland <rjmoerland@bd5d69a4-6419-0410-a1cd-843bcb727202>
2010-08-21 20:51:34 +0000

    Fixed a clash of the parent process and a child process when accessing the config file. Should fix the issue with saving the config file when running Disk Cleaner from a thumb drive.

--- src/gui/version.h
+++ src/gui/version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "15";
+	static const char DATE[] = "21";
 	static const char MONTH[] = "08";
 	static const char YEAR[] = "2010";
 	static const char UBUNTU_VERSION_STYLE[] = "10.08";
@@ -16,14 +16,14 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 8;
-	static const long BUILD = 1767;
-	static const long REVISION = 9599;
+	static const long BUILD = 1768;
+	static const long REVISION = 9603;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 3189;
-	#define RC_FILEVERSION 1,8,1767,9599
-	#define RC_FILEVERSION_STRING "1, 8, 1767, 9599\0"
-	static const char FULLVERSION_STRING[] = "1.8.1767.9599";
+	static const long BUILDS_COUNT = 3190;
+	#define RC_FILEVERSION 1,8,1768,9603
+	#define RC_FILEVERSION_STRING "1, 8, 1768, 9603\0"
+	static const char FULLVERSION_STRING[] = "1.8.1768.9603";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 0;

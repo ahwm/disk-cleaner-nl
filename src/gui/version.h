6b89c87b1b0979eeb240165c182cbd2931379c31
rjmoerland <rjmoerland@bd5d69a4-6419-0410-a1cd-843bcb727202>
2010-09-11 20:10:46 +0000

    Fixed a bug in the expansion of strings. If a %string% wasn't in the list of environment variables, the path was never expanded correctly.

--- src/gui/version.h
+++ src/gui/version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "28";
-	static const char MONTH[] = "08";
+	static const char DATE[] = "11";
+	static const char MONTH[] = "09";
 	static const char YEAR[] = "2010";
-	static const char UBUNTU_VERSION_STYLE[] = "10.08";
+	static const char UBUNTU_VERSION_STYLE[] = "10.09";
 	
 	//Software Status
 	static const char STATUS[] = "Beta";
@@ -16,14 +16,14 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 7;
-	static const long BUILD = 1629;
-	static const long REVISION = 8875;
+	static const long BUILD = 1633;
+	static const long REVISION = 8899;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 2961;
-	#define RC_FILEVERSION 1,7,1629,8875
-	#define RC_FILEVERSION_STRING "1, 7, 1629, 8875\0"
-	static const char FULLVERSION_STRING[] = "1.7.1629.8875";
+	static const long BUILDS_COUNT = 2964;
+	#define RC_FILEVERSION 1,7,1633,8899
+	#define RC_FILEVERSION_STRING "1, 7, 1633, 8899\0"
+	static const char FULLVERSION_STRING[] = "1.7.1633.8899";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 0;

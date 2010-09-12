a64584d78b5aaf56146f068f3284fb2a8392445d
rjmoerland <rjmoerland@bd5d69a4-6419-0410-a1cd-843bcb727202>
2010-09-12 23:25:13 +0000

    updated code to remove compiler warnings

--- src/gui/version.h
+++ src/gui/version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "11";
+	static const char DATE[] = "13";
 	static const char MONTH[] = "09";
 	static const char YEAR[] = "2010";
 	static const char UBUNTU_VERSION_STYLE[] = "10.09";
@@ -16,14 +16,14 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 9;
-	static const long BUILD = 1783;
-	static const long REVISION = 9689;
+	static const long BUILD = 1784;
+	static const long REVISION = 9694;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 3223;
-	#define RC_FILEVERSION 1,9,1783,9689
-	#define RC_FILEVERSION_STRING "1, 9, 1783, 9689\0"
-	static const char FULLVERSION_STRING[] = "1.9.1783.9689";
+	static const long BUILDS_COUNT = 3224;
+	#define RC_FILEVERSION 1,9,1784,9694
+	#define RC_FILEVERSION_STRING "1, 9, 1784, 9694\0"
+	static const char FULLVERSION_STRING[] = "1.9.1784.9694";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 0;

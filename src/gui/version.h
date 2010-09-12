d96ec255a55f1cbfe300b631c0f25841f5e65990
rjmoerland <rjmoerland@bd5d69a4-6419-0410-a1cd-843bcb727202>
2010-09-12 22:08:08 +0000

    Cleaning up code in ExpandEnvironmentString, plus fixing a little (unreleased) bug in getting the path of %string%, if not in environment block.

--- src/gui/version.h
+++ src/gui/version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "11";
+	static const char DATE[] = "13";
 	static const char MONTH[] = "09";
 	static const char YEAR[] = "2010";
 	static const char UBUNTU_VERSION_STYLE[] = "10.09";
@@ -16,14 +16,14 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 7;
-	static const long BUILD = 1633;
-	static const long REVISION = 8899;
+	static const long BUILD = 1645;
+	static const long REVISION = 8960;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 2964;
-	#define RC_FILEVERSION 1,7,1633,8899
-	#define RC_FILEVERSION_STRING "1, 7, 1633, 8899\0"
-	static const char FULLVERSION_STRING[] = "1.7.1633.8899";
+	static const long BUILDS_COUNT = 2981;
+	#define RC_FILEVERSION 1,7,1645,8960
+	#define RC_FILEVERSION_STRING "1, 7, 1645, 8960\0"
+	static const char FULLVERSION_STRING[] = "1.7.1645.8960";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 0;
